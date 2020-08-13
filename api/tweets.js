1ff0925cf9d5d551f9516633116bdc84ea946a2c
Mayank Pathela <mp.pathela@gmail.com>
2020-08-13 23:37:40 +0530

    test html-to-img

--- api/tweets.js
+++ api/tweets.js
@@ -1,4 +1,20 @@
+import * as htmlToImage from 'html-to-image';
+import { toPng } from 'html-to-image';
+
+var node = <div>Hello World</div>
+
+
 module.exports = (req, res) => {
-    const { name = 'World' } = req.query
-    res.send(`Hello ${name}!`)
+    htmlToImage.toPng(node)
+        .then(function (dataUrl) {
+            var img = new Image();
+            img.src = dataUrl;
+            // document.body.appendChild(img);
+            res.send(img);
+        })
+        .catch(function (error) {
+            console.error('oops, something went wrong!', error);
+        });
+    // const { name = 'World' } = req.query
+    // res.send(`Hello ${name}!`)
 }
