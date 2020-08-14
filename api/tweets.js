a0e1b82958f06c0720e56667cdb5a4954ce8d126
Mayank Pathela <mp.pathela@gmail.com>
2020-08-15 00:12:55 +0530

    check

--- api/tweets.js
+++ api/tweets.js
@@ -1,26 +1,20 @@
-// import domtoimage from 'dom-to-image';
-// var node = document.createElement('html');
-// node.innerHTML('<div>Hello World</div>');
-const { readFileSync } = require('fs');
-const { join } = require('path');
+var express = require('express');
+var app = express();
+var fs = require('fs');
 
-const file = readFileSync(join(__dirname, 'api', 'one.png'), 'base64')
+app.get('/', function (req, res) {
+    // fs.readFile('one.png', function (err, data) {
+    //     if (err) throw err;
+    //     res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+    //     res.end(data)
+    // })
+    res.send('Hello World');
 
+})
 
-module.exports = (req, res) => {
-    
-    // domtoimage.toPng(node)
-    //     .then(function (dataUrl) {
-    //         download(dataUrl, 'my-node.png');
-    //         var img = new Image();
-    //         img.src = dataUrl;
-    //         document.body.appendChild(img);
-    //         res.send(node);
-    //     })
-    //     .catch(function (error) {
-    //         console.error('oops, something went wrong!', error);
-    //     });    
-    // const { name = 'World' } = req.query
-        // res.send('Hello World');
-    res.send(contents)
-}
+// var server = app.listen(8081, function () {
+//     var host = server.address().address
+//     var port = server.address().port
+
+//     console.log("Example app listening at http://%s:%s", host, port)
+// })
