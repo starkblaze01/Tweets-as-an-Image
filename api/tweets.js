bea382bbcb46fba87b68511c8adc795a336426b4
Mayank Pathela <mp.pathela@gmail.com>
2020-08-15 00:24:20 +0530

    read image from source

--- api/tweets.js
+++ api/tweets.js
@@ -1,20 +1,6 @@
 var express = require('express');
-var app = express();
-var fs = require('fs');
-
-app.get('/', function (req, res) {
-    // fs.readFile('one.png', function (err, data) {
-    //     if (err) throw err;
-    //     res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-    //     res.end(data)
-    // })
-    res.send('Hello World');
-
-})
-
-// var server = app.listen(8081, function () {
-//     var host = server.address().address
-//     var port = server.address().port
-
-//     console.log("Example app listening at http://%s:%s", host, port)
-// })
+var image = new Image();
+image.src = 'https://cnet2.cbsistatic.com/img/bZaqv6tPvT44-cop4ZL2gG3j5wE=/940x0/2020/01/17/7da55a03-ac5b-4ec1-b59b-6b3c2414e68b/egdt5idw4aittju.jpg'
+module.exports = (req, res) => {    
+    res.send(image)
+}
