14c5666d702101d92db5eec6cc8ff103e3a17832
Mayank Pathela <mp.pathela@gmail.com>
2020-08-15 00:31:58 +0530

    read image from source

--- api/tweets.js
+++ api/tweets.js
@@ -1,6 +1,14 @@
 var express = require('express');
-var image = new Image();
-image.src = 'https://cnet2.cbsistatic.com/img/bZaqv6tPvT44-cop4ZL2gG3j5wE=/940x0/2020/01/17/7da55a03-ac5b-4ec1-b59b-6b3c2414e68b/egdt5idw4aittju.jpg'
+const request = require('request');
+
+let url = 'https://cnet2.cbsistatic.com/img/bZaqv6tPvT44-cop4ZL2gG3j5wE=/940x0/2020/01/17/7da55a03-ac5b-4ec1-b59b-6b3c2414e68b/egdt5idw4aittju.jpg'
 module.exports = (req, res) => {    
-    res.send(image)
+    let lol = ''
+    request({ url, encoding: null }, (err, resp, buffer) => {
+        // Use the buffer
+        // buffer contains the image data
+        // typeof buffer === 'object'
+        lol = resp
+    });
+    res.send(lol)
 }
