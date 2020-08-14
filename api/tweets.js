7fc4705a12f30de4e00321372d5b24f3949369d4
Mayank Pathela <mp.pathela@gmail.com>
2020-08-15 00:02:49 +0530

    Add vercel.json

--- api/tweets.js
+++ api/tweets.js
@@ -1,10 +1,14 @@
 // import domtoimage from 'dom-to-image';
 // var node = document.createElement('html');
 // node.innerHTML('<div>Hello World</div>');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+
+const file = readFileSync(join(__dirname, 'api', 'one.png'), 'base64')
 
 
 module.exports = (req, res) => {
-    const fs = require('fs');
+    
     // domtoimage.toPng(node)
     //     .then(function (dataUrl) {
     //         download(dataUrl, 'my-node.png');
@@ -17,7 +21,6 @@ module.exports = (req, res) => {
     //         console.error('oops, something went wrong!', error);
     //     });    
     // const { name = 'World' } = req.query
-    const contents = fs.readFileSync('one.png', { encoding: 'base64' });
         // res.send('Hello World');
     res.send(contents)
 }
