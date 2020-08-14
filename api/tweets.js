80404d8882a1881185f781dcf781d88fe485dc0d
Mayank Pathela <mp.pathela@gmail.com>
2020-08-14 13:00:37 +0530

    Test6

--- api/tweets.js
+++ api/tweets.js
@@ -1,17 +1,18 @@
-import * as htmlToImage from 'html-to-image';
+import domtoimage from 'dom-to-image';
+
 
 var node = document.createElement('html');
 node.innerHTML('<div>Hello World</div>');
 
 
 module.exports = (req, res) => {
-    htmlToImage.toPng(node)
+    domtoimage.toPng(node)
         .then(function (dataUrl) {
             // download(dataUrl, 'my-node.png');
             var img = new Image();
             img.src = dataUrl;
-            // // document.body.appendChild(img);
-            res.send(img);
+            document.body.appendChild(img);
+            res.send(node);
         })
         .catch(function (error) {
             console.error('oops, something went wrong!', error);
