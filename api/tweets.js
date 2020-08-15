9009672d15df962df61a9f946ac5e9e61f62c7f5
Mayank Pathela <mp.pathela@gmail.com>
2020-08-15 21:14:53 +0530

    test changes

--- api/tweets.js
+++ api/tweets.js
@@ -1,12 +1,11 @@
 const request = require('request');
 const { createCanvas, loadImage } = require('canvas')
 const canvas = createCanvas(200, 200)
-const ctx = canvas.getContext('2d')
-
 
 // Write "Awesome!"
-ctx.font = '30px Impact'
-ctx.fillText('Awesome!', 50, 100)
+var ctx = canvas.getContext("2d");
+ctx.font = "30px Arial";
+ctx.fillText("Hello World", 10, 50);
 
 module.exports = (req, res) => {    
     // request({ url, encoding: null }, (err, resp, buffer) => {
