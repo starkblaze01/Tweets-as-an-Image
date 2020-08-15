7db9c16c6c5050795104b90d787d470524d8127d
Mayank Pathela <mp.pathela@gmail.com>
2020-08-15 22:05:30 +0530

    change font

--- api/tweets.js
+++ api/tweets.js
@@ -1,11 +1,14 @@
 const request = require('request');
-const { createCanvas, loadImage } = require('canvas')
-const canvas = createCanvas(200, 200)
+const { createCanvas, loadImage, registerFont } = require('canvas')
+registerFont('comicsans.ttf', { family: 'Comic Sans' })
+
+const canvas = createCanvas(500, 500)
+
 
 // Write "Awesome!"
 var ctx = canvas.getContext("2d");
-ctx.font = "30px Arial";
-ctx.fillText("Hello World", 10, 50);
+ctx.font = '12px "Comic Sans"'
+ctx.fillText('Everyone hates this font :(', 250, 10)
 
 module.exports = (req, res) => {    
     // request({ url, encoding: null }, (err, resp, buffer) => {
