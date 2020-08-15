5e462e0a94f4b154cf92b0bb5b09a9c0a36ec985
Mayank Pathela <mp.pathela@gmail.com>
2020-08-15 19:44:02 +0530

    change text

--- api/tweets.js
+++ api/tweets.js
@@ -6,16 +6,8 @@ const ctx = canvas.getContext('2d');
 
 // Write "Awesome!"
 ctx.font = '30px Impact'
-ctx.rotate(0.1)
 ctx.fillText('Awesome!', 50, 100)
 
-// Draw line under text
-var text = ctx.measureText('Awesome!')
-ctx.strokeStyle = 'rgba(0,0,0,0.5)'
-ctx.beginPath()
-ctx.lineTo(50, 102)
-ctx.lineTo(50 + text.width, 102)
-ctx.stroke()
 
 module.exports = (req, res) => {    
     // request({ url, encoding: null }, (err, resp, buffer) => {
