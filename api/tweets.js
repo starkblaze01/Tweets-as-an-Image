8b9f74a6b67f8c087e40177c5e62c7cb59611b1d
Mayank Pathela <mp.pathela@gmail.com>
2020-08-15 20:55:16 +0530

    redeploy canvas

--- api/tweets.js
+++ api/tweets.js
@@ -1,17 +1,16 @@
 const request = require('request');
 const { createCanvas, loadImage } = require('canvas')
 const canvas = createCanvas(200, 200)
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d')
 
 
 // Write "Awesome!"
 ctx.font = '30px Impact'
 ctx.fillText('Awesome!', 50, 100)
 
-
 module.exports = (req, res) => {    
     // request({ url, encoding: null }, (err, resp, buffer) => {
-        res.send(canvas.toBuffer())
+    res.send(canvas.toBuffer())
     // });
     
 }
