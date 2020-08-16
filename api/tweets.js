bb7cccc95fdc15d6281cd0f364b0318901d3b423
Mayank Pathela <mp.pathela@gmail.com>
2020-08-16 11:25:40 +0530

    check file path

--- api/tweets.js
+++ api/tweets.js
@@ -1,17 +1,22 @@
 const request = require('request');
 const Jimp = require('jimp')
 let image = new Jimp(256, 256)
+const fs = require('fs');
 
-image.quality(60)
-Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(font => {
-    image.print(font, 10, 10, 'Hello \n world!');
-});
+
+// image.quality(60)
+// Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(font => {
+//     image.print(font, 10, 10, 'Hello \n world!');
+// });
 
 module.exports = (req, res) => {    
-    // request({ url, encoding: null }, (err, resp, buffer) => {
-    image.getBuffer(Jimp.MIME_PNG, function (err, src) {
-        res.send(src)
+    fs.readdir('.', function (err, files) {
+        res.send(files)        
     })
+    // request({ url, encoding: null }, (err, resp, buffer) => {
+    // image.getBuffer(Jimp.MIME_PNG, function (err, src) {
+        
+    // })
     // });
     
 }
