66e146f037715a43ca225a4847a444ebbc226c5e
Mayank Pathela <mp.pathela@gmail.com>
2020-08-16 11:41:08 +0530

    change package.json path

--- api/tweets.js
+++ api/tweets.js
@@ -4,19 +4,16 @@ let image = new Jimp(256, 256)
 const fs = require('fs');
 
 
-// image.quality(60)
-// Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(font => {
-//     image.print(font, 10, 10, 'Hello \n world!');
-// });
+image.quality(60)
+Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(font => {
+    image.print(font, 10, 10, 'Hello \n world!');
+});
 
 module.exports = (req, res) => {    
-    fs.readdir('./api/node_modules/@jimp/plugin-print', function (err, files) {
-        res.send(files)        
-    })
     // request({ url, encoding: null }, (err, resp, buffer) => {
-    // image.getBuffer(Jimp.MIME_PNG, function (err, src) {
-        
-    // })
+    image.getBuffer(Jimp.MIME_PNG, function (err, src) {
+        res.send(src)
+    })
     // });
     
 }
